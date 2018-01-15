9ac79850a640250ed870a185e3928570b354a2c7
wvfeng <wvfeng@163.com>
2018-01-15 18:46:43 +0800

    添加““个人”接口

--- Application/Mobile/Controller/MyinfoController.class.php
+++ Application/Mobile/Controller/MyinfoController.class.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace Mobile\Controller;
-
 /**
  * 个人信息控制器
  */
 class MyinfoController extends CommonController
 {
-    //测试使用
-    public function index(){
-        print_r($this->Model);
+    //获取个人数据
+    public function getIndexInfo($id = null){
+        if(empty($id)) $this->returnAjaxError(['message'=>'用户ID不能为空！']);
+        $this->quickReturn($this->Model->getIndexInfo($id));
+    }
+
+    public function test($str){
+        echo url_encode($str).PHP_EOL;
+        echo url_decode(url_encode($str));
     }
 }
--- Application/Mobile/Model/MyinfoModel.class.php
+++ Application/Mobile/Model/MyinfoModel.class.php
@@ -7,8 +7,28 @@ namespace Mobile\Model;
  * @package Home\Model
  * 用户模型库
  */
-
-class MyinfoModel extends CommonModel
+use Think\Model\RelationModel;
+class MyinfoModel extends RelationModel
 {
     protected $tableName = 'User';
+    protected $_link = [
+        'User_detail'=> [
+            'mapping_type'  => self::HAS_ONE,
+            'class_name'    => 'user_detail',
+            'foreign_key'   => 'user_id',
+            'as_fields'     => 'nickname,headimgurl,mood,sex,surplus_int,country,province,city'
+            ],
+    ];
+
+    //获取个人数据
+    public function getIndexInfo($id){
+        if(empty($id)) return false;
+        $data = $this->field('iphone,email,password',true)->relation('User_detail')->find($id);
+        $data['Order'] = array_shift(M('Order')->field('count(id) no_pay')->where(['user_id'=>$id,'type'=>'0'])->select());
+        $data['Order'] += array_shift(M('Order')->field('count(id) no_shipments')->where(['user_id'=>$id,'type'=>'1'])->select());
+        $data['Order'] += array_shift(M('Order')->field('count(id) on_shipments')->where(['user_id'=>$id,'type'=>'2'])->select());
+        $data['Order'] += array_shift(M('Order')->field('count(id) no_appraise')->where(['user_id'=>$id,'type'=>'3'])->select());
+        $data['Order'] += array_shift(M('Order')->field('count(id) on_after')->where(['user_id'=>$id,'type'=>['GT','4']])->select());
+        return $data;
+    }
 }
