35c2ce9dbe09722d5f7463556b093fe3490f3d04
wvfeng <wvfeng@163.com>
2018-01-17 11:09:33 +0800

    引入分页Trait

--- Application/Common/Model/CollectModel.class.php
+++ Application/Common/Model/CollectModel.class.php
@@ -11,8 +11,8 @@ namespace Common\Model;
 class CollectModel extends CommonModel
 {
     protected $_link = [
-        'User_detail'=> [
-            'mapping_type'  => self::HAS_ONE,
+        'shop'=> [
+            'mapping_type'  => self::BELONGS_TO,
             'class_name'    => 'shop',
             'foreign_key'   => 's_id',
             'as_fields'     => 'id,tit,tit_en,price,rate'
@@ -22,4 +22,9 @@ class CollectModel extends CommonModel
     protected $_map = [
         //'表单' => '字段名'
     ];
+
+    public function getList($id){
+        print_r($this->where(['u_id'=>1])->relation('shop')->limit($this->page())->select());
+        print_r($this->_sql());die;
+    }
 }
