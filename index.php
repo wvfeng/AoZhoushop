45a1d1cc22fa841ec626874e6e8286da3fe07957
wvfeng <wvfeng@163.com>
2018-01-30 14:06:33 +0800

    设置BUILD_LITE_FILE

--- index.php
+++ index.php
@@ -16,11 +16,16 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
+define('BUILD_LITE_FILE',!APP_DEBUG);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
 
 // 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
+if(defined('BUILD_LITE_FILE') && BUILD_LITE_FILE && file_exists(APP_PATH.'Runtime/lite.php')){
+    require APP_PATH.'Runtime/lite.php';
+}else{
+    require './ThinkPHP/ThinkPHP.php';
+}
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
