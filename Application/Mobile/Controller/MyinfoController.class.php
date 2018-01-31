d1e4e8e744ad26cfd3a17bfb7cd3d5531bea8aa3
wvfeng <wvfeng@163.com>
2018-01-31 19:12:35 +0800

    优化收藏控制器

--- Application/Mobile/Controller/MyinfoController.class.php
+++ Application/Mobile/Controller/MyinfoController.class.php
@@ -10,9 +10,8 @@ class MyinfoController extends CommonController
      * 获取个人数据
      * @param null $id
      */
-    public function getUserInfo($id = null){
-        if(empty($id)) $this->returnAjaxError(['message'=>'用户ID不能为空！']);
-        $this->quickReturn($this->Model->getUserInfo($id));
+    public function getUserInfo(){
+        $this->quickReturn($this->Model->getUserInfo($this->UserID));
     }
 
     /**
@@ -20,6 +19,7 @@ class MyinfoController extends CommonController
      */
     public function UpdateUserInfo(){
         $this->Model->create();
+        $this->id = $this->UserID;
         //$this->id = url_decode($this->id);  //解密用户ID
         $this->quickReturn($this->Model->save(),'修改');
     }
@@ -31,20 +31,19 @@ class MyinfoController extends CommonController
      * @param null $ShopID
      * @param null $CollectID
      */
-    public function Collect($UserID =null,$type = 'list',$ShopID = null,$CollectID = null){
+    public function Collect($type = 'list',$ShopID = null,$CollectID = null){
         if(empty($type)) $this->returnAjaxError(['message'=>'操作类型不能为空！']);
         $model = D('Common/Collect');
         $type = strtolower($type);
         //if(!empty($UserID)) $UserID = url_decode($UserID);
         if($type == 'list'){
-            if(empty($UserID)) $this->returnAjaxError(['message'=>'缺少用户ID!']);
-            $this->quickReturn($model->getList($UserID));
+            $this->quickReturn($model->getList($this->UserID));
         }elseif($type == 'add'){
-            if(empty($UserID) || empty($ShopID)) $this->returnAjaxError(['message'=>'缺少用户或商品ID!']);
-            $this->quickReturn($model->addCollect($UserID,$ShopID),'收藏');
+            if(empty($ShopID)) $this->returnAjaxError(['message'=>'缺少商品ID!']);
+            $this->quickReturn($model->addCollect($this->UserID,$ShopID),'收藏');
         }elseif(in_array($type,['delete','remove'])){
-            if(empty($UserID) || empty($ShopID)) $this->returnAjaxError(['message'=>'缺少用户或商品ID!']);
-            $this->quickReturn($model->removeCollect($UserID,$ShopID),'删除收藏');
+            if(empty($ShopID)) $this->returnAjaxError(['message'=>'缺少商品ID!']);
+            $this->quickReturn($model->removeCollect($this->UserID,$ShopID),'删除收藏');
         }else{
             $this->returnAjaxError(['message'=>'空类型！']);
         }
@@ -57,12 +56,13 @@ class MyinfoController extends CommonController
      * @param null $type
      * @param null $content
      */
-    public function creditOrder($UserID =null,$ShopID = null,$type = null,$content = null){
-        if(empty($UserID) || empty($ShopID)) $this->returnAjaxError(['message'=>'缺少用户或商品ID!']);
+    public function creditOrder($ShopID = null,$type = null,$content = null){
+        if(empty($ShopID)) $this->returnAjaxError(['message'=>'缺少商品ID!']);
         if(empty($type)) $this->returnAjaxError(['message'=>'操作类型不能为空!']);
         if(in_array(strtolower($type),['退货','换货','售后','1','2','3'])) $this->returnAjaxError(['message'=>'类型错误!']);
         $model = D('Credit');
         $model->create();
+        $model->id = $this->UserID;
         $this->quickReturn($model->add(),'申请'.$type);
     }
 
