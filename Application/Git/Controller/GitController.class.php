afdd11f4a03b31e2ebc5e4ac93b9f27be673784a
wvfeng <wvfeng@163.com>
2018-01-31 13:08:13 +0800

    设置钩子脚本

--- Application/Git/Controller/GitController.class.php
+++ Application/Git/Controller/GitController.class.php
@@ -8,10 +8,15 @@ class GitController
 {
     private $ProjectPath = '/bweb/azshop'; // 生产环境web目录
     private $token = '89d9a100-0633-11e8-8d23-b19745d39c75';
+    private $save = [
+        'ref','before','after','created','deleted','forced','base_ref','compare','commits'
+    ];
     public function Push(){
-        $input = json_decode(file_get_contents('php://input'), true);
-
         if (!empty($_SERVER['X-GitHub-Event']) || $_SERVER['X-GitHub-Event'] == 'push') {
+            $json = json_decode(file_get_contents('php://input'), true);
+            foreach ($this->save as $key){
+                $input[$key] = $json[$key];
+            }
             static::writeLog($input);
             $cmd = "cd {$this->ProjectPath} && git pull;";
 
