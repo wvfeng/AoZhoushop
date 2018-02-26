f77281dc769890a3f04afd10925b81660725c84a
chen chong <2379920898@qq.com>
2018-02-26 15:26:46 +0800

    0

--- Application/Computer/Controller/OrderController.class.php
+++ Application/Computer/Controller/OrderController.class.php
@@ -5,10 +5,22 @@ class OrderController extends CommonController
 {	
     //我的订单
     public function myOrder(){
+        /*$userId = url_decode(I('userId'));*/
         $userId = I('userId');
-        $order = M('order')->where(['user_id'=>$userId])->select();
-        foreach ($order as $key => &$v) {
-            
+        if(I('type')){
+            $type = ['type'=>I('type')];
         }
+        $data['data'] = M('order')->where(['user_id'=>$userId,'status'=>1])->where($type)->limit($this->page())
+        ->field('shop_id,user_id,num,date,type,money,paymoney,id')->select();
+        foreach ($data['data'] as $key => &$v) {
+            $shopId = explode('|*|',$v['shop_id']);
+            $shopNum = explode('|*|',$v['num']);
+            $map['id'] = array('in',$shopId);
+            $v['shop'] = M('shop')->where($map)->field('img,tit,tit_en,price')->select();
+            foreach ($v['shop'] as $keys => &$vs) {
+                $vs['paynum'] = $shopNum[$keys];
+            }
+        }
+        $this->quickReturn($data);
     } 
 }
