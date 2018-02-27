ac8c74fda02768a4dca588a91c246cb5755e5a7b
wvfeng <wvfeng@163.com>
2018-02-27 13:32:26 +0800

    封装图片上传方法

--- Application/Common/Model/CommonModel.class.php
+++ Application/Common/Model/CommonModel.class.php
@@ -12,6 +12,8 @@ use Think\Model\RelationModel;;
 
 class CommonModel extends RelationModel
 {
+    public $Error = null;
+
     use \Common\Controller\page;
 
     //根据数据表获取全部字段信息
@@ -26,7 +28,7 @@ class CommonModel extends RelationModel
         return trim($list,",");
     }
 
-    //封装图片上传方法
+    //封装图片上传方法bas64
     public function uploadImg($image,$path){
 
         //base64转换
@@ -45,6 +47,32 @@ class CommonModel extends RelationModel
             return $imagePath;
         }else{
             return json_decode(['data'=>null,"code"=>0,"msg"=>"图片生成失败"]);
+        }
+    }
+
+    public $ImagePathName;
+    public $ImagePathName_thumb;
+    //图片上传from-data
+    public function uploadImage($image,$path,$thumb = false,$thumb_w = 150,$thumb_h = 150){
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize   =     3145728 ;// 设置附件上传大小
+        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath  =      './'; // 设置附件上传根目录
+        $upload->savePath  =      $path; // 设置附件上传目录    // 上传单个文件
+        $upload->subName   =    array('date','Ymd');
+        $info   =   $upload->uploadOne($image);
+        if(!$info) {// 上传错误提示错误信息
+            $this->Error = $upload->getError();
+            return false;
+        }else{// 上传成功 获取上传文件信息
+            if($thumb){
+                //生成缩略图
+                $this->ImagePathName = $info['savepath'].$info['savename'];
+                $image = new \Think\Image(\Think\Image::IMAGE_GD,$this->ImagePathName);
+                $this->ImagePathName_thumb = dirname($this->ImagePathName).'/thumb_'.basename($this->ImagePathName);
+                $image->thumb($thumb_w, $thumb_h)->save($this->ImagePathName_thumb);
+            }
+            return $this->ImagePathName;
         }
     }
 
