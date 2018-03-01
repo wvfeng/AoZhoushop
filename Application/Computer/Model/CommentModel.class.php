d922afad9070401b8ddb616e554eff6592eb9b43
wvfeng <wvfeng@163.com>
2018-03-01 16:22:40 +0800

    修改命名空间

---
+++ Application/Computer/Model/CommentModel.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Computer\Model;
+
+/**
+ * Class CommentModel
+ * @package Computer\Model
+ */
+class CommentModel extends CommonModel
+{
+    public function CommentAdd(ShopController $shop){
+        var_dump($shop);
+    }
+}
