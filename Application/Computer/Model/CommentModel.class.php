b8f76811f1e6f9908ece2e7b0eaf06be0ed03931
wvfeng <wvfeng@163.com>
2018-03-01 18:23:28 +0800

    解决商品控制器代码丢失问题

--- Application/Computer/Model/CommentModel.class.php
+++ Application/Computer/Model/CommentModel.class.php
@@ -8,6 +8,7 @@ namespace Computer\Model;
  */
 class CommentModel extends CommonModel
 {
+    protected $trueTableName = 'mall_shop_comment';
     public function CommentAdd(ShopController $shop){
         var_dump($shop);
     }
