7cd57c4b42844f43c6cf0cf0d5b11385dcffc871
wvfeng <wvfeng@163.com>
2018-03-08 14:55:50 +0800

    添加获取默认收货地址接口

--- Application/Common/Controller/AddressController.class.php
+++ Application/Common/Controller/AddressController.class.php
@@ -4,10 +4,15 @@ namespace Common\Controller;
 class AddressController extends CommonController
 {
     public $message = '收货地址';
+    private $where = [];
 
     public function manage($type = 'list',$id = null){
-        $where = ['user_id'=>$this->userId];
-        if(is_numeric($id)) $where['id'] = $id;
+        $this->where['user_id'] = $this->userId;
+        if($type == 'getdefault'){
+            $this->where['sdefault'] = 1;
+        }else{
+            if(is_numeric($id)) $this->where['id'] = $id;
+        }
         switch (strtolower($type)){
             case 'add':
                 $message = '添加';
@@ -26,7 +31,7 @@ class AddressController extends CommonController
                 break;
             case 'remove':
                 $message = '删除';
-                $res = $this->Model->where($where)->delete();
+                $res = $this->Model->where($this->where)->delete();
                 $res = $res === false ? $res:true;
                 break;
             case 'delete':
@@ -35,7 +40,7 @@ class AddressController extends CommonController
             case 'update':
                 $message = '更新';
                 $data = $this->Model->create();
-                $res = $this->Model->where($where)->save($data);
+                $res = $this->Model->where($this->where)->save($data);
                 if($res === false){
                     $message .= $this->message;
                     if($this->Model->Error) $message = $this->Model->Error;
@@ -44,16 +49,19 @@ class AddressController extends CommonController
                 break;
             case 'list':
                 $message = '查询';
-                $res = $this->Model->where($where)->order('id desc')/*->limit($this->page())*/->select();
+                $res = $this->Model->where($this->where)->order('id desc')/*->limit($this->page())*/->select();
                 if($res === false){
                     $this->quickReturn(false,$message == 'Error' ? '错误类型！操作':$message.$this->message);
                 }else{
                     $this->returnAjaxSuccess(['data'=>$res,'message'=>$message.$this->message.'成功！']);
                 }
                 break;
+            case 'getdefault':
+                $this->manage($type = 'list',$id);
+                break;
             case 'default':
                 $message = '设置默认地址';
-                $res = $this->Model->setDefault($where);
+                $res = $this->Model->setDefault($this->where);
                 break;
             default :
                 $message = 'Error';
@@ -63,7 +71,7 @@ class AddressController extends CommonController
     }
 
     //可选的操作
-    private $actions = ['add','list','delete','remove','update','default'];
+    private $actions = ['add','list','delete','remove','update','default','getdefault'];
     public function _empty($method, $args){
         if(in_array(strtolower($method),$this->actions)) $this->CallAction($method);
         parent::_empty();
