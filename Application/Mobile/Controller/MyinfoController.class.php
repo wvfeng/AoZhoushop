be26c5b6705ec685ee1be8be02dd20489a466518
wvfeng <wvfeng@163.com>
2018-03-12 15:48:52 +0800

    测试FormData数据

--- Application/Mobile/Controller/MyinfoController.class.php
+++ Application/Mobile/Controller/MyinfoController.class.php
@@ -27,7 +27,7 @@ class MyinfoController extends CommonController
             if($fileName === null) $this->returnAjaxError(['message'=>$this->Model->Error]);
             $data['User_detail']['headimgurl'] = $fileName;
         }
-        $this->returnAjaxError(['data'=>$_SERVER]);
+        $this->returnAjaxError(['data'=>$_FILES]);
         $res = $this->Model->where(['id'=>$this->userId])->relation(true)->save($data);
         if($res === false){
             //修改失败，资源回收
