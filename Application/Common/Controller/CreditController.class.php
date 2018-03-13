0f3f016ed1d31eb50b71b2d37d02084d66137d2d
wvfeng <wvfeng@163.com>
2018-03-13 11:07:26 +0800

    修改时间格式

--- Application/Common/Controller/CreditController.class.php
+++ Application/Common/Controller/CreditController.class.php
@@ -25,6 +25,7 @@ class CreditController extends CommonController
         if(is_numeric($type)) $type = $types[$type];
         $data = $this->Model->create();
         $data['u_id'] = $this->userId;
+        exit(json_encode(['data'=>explode(',',I('post.img'))]));
         $this->quickReturn($this->Model->add($data),'申请'.$type);
     }
 
@@ -44,6 +45,9 @@ class CreditController extends CommonController
     public function creditList(){
         $fields = ['update_time'];
         $data = $this->Model->field($fields,true)->where(['u_id'=>$this->userId])->relation('shop')->limit($this->page())->select();
+        foreach ($data as $key=>$value){
+            $data[$key]['create_time'] = date('Y-m-d',$data[$key]['create_time']);
+        }
         $this->quickReturn($data,'查询');
     }
 
